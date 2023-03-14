33d7fe7f3a3ee574bfcf8a4f3f9b55aaa8bf14a0
William Diakite <william.diakite@gmail.com>
2023-03-14 11:12:30 -0400

    add test for i18n

--- src/test/i18n.test.ts
+++ src/test/i18n.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import translations from '$lib/i18n/translations';
 
-describe('Translations', () => {
+describe('translations', () => {
     it('same number of items', () => {
         const ls = Object.keys(translations).map(l => Object.keys(translations[l]).length)
         expect(ls.every(l => l === ls[0])).toEqual(true)
