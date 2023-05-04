bcfa04f49008178945c3dc9ecd6ade44c8e17dc0
William Diakite <william.diakite@gmail.com>
2023-05-04 08:39:51 -0400

    introduce css

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Project } from '$lib/types/Markdown';
+	export let project: Project;
+</script>
+
+<div class="stack stack-small">
+	<h3 class="fs:600">{project.title}</h3>
+	<ul class="hstack">
+		{#each project.tags as t}
+			<li class="fs:300 ff:secondary clr:red">{t}</li>
+		{/each}
+	</ul>
+	<p class="fs:500 fw:light">{project.description}</p>
+	<span class="">Depuis {project.since}</span>
+</div>
--- src/lib/i18n/translations.ts
+++ src/lib/i18n/translations.ts
@@ -1,12 +1,17 @@
 const translations: { [locale: string]: { [path: string]: string } } = {
     'en': {
         // ARIA labels
-        'aria.home.title': "Lab Ouvroir's homepage",
+        'aria.home.title': "Homepage",
         'aria.nav.label': 'main navigation',
         'aria.locales.label': 'language menu',
         'aria.locales.fr': 'french',
         'aria.locales.en': 'english',
         'aria.projects.carousel': 'projects carousel',
+
+        // Header
+        'header.subtitle': 'a <span class="clr:red">digital</span> art history and museology lab.',
+        'header.description': "L’Ouvroir is a Digital Art History and Museology lab at the Université de Montréal dedicated to support the work conducted within the framework of the Partnership « New Uses of Collections in Art Museums »",
+
 
         // Navigation bar
         'nav.home': 'Home',
@@ -30,21 +35,32 @@ const translations: { [locale: string]: { [path: string]: string } } = {
         'home.projects.title': 'Latest projects & publications',
         'home.events.title': 'Upcoming events',
 
+        // About
+        'about.title': 'About',
+
         // Events
         'events.title': "Our events",
-
 
         // Error
         'error.title': "Oups! The page your asking for cannot be find."
     },
     'fr': {
         // ARIA labels
-        'aria.home.title': "Page d'accueil du laboratoire Ouvroir",
+        'aria.home.title': "Accueil",
+        'aria.projects.title': 'Projets',
+        'aria.blog.title': 'Blog',
+        'aria.about.title': 'À propos',
+        'aria.team.title': 'Membres',
+        'aria.events.title': 'Événements',
         'aria.nav.label': 'navigation principale',
         'aria.locales.label': 'menu langues',
         'aria.locales.fr': 'français',
         'aria.locales.en': 'anglais',
         'aria.projects.carousel': 'carousel des projets',
+
+        // Header
+        'header.subtitle': 'd’histoire de l’art et de muséologie <span class="clr:red">numériques</span>.',
+        'header.description': "L’Ouvroir d’histoire de l’art et de muséologie numérique de l’Université de Montréal, est un laboratoire de recherche destiné à soutenir le travail conduit dans le cadre du Partenariat « Des nouveaux usages des collections dans les musées d’art »",
 
         // Navigation bar
         'nav.home': 'Accueil',
@@ -68,6 +84,9 @@ const translations: { [locale: string]: { [path: string]: string } } = {
         // Homepage
         'home.projects.title': 'Projets & publications',
         'home.events.title': 'Nos événements',
+
+        // About
+        'about.title': 'À propos',
 
         // Events
         'events.title': "Nos évenements",
