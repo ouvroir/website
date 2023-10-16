5d073766f9948da2c9e97d1f827cef28aae3df16
William Diakite <william.diakite@gmail.com>
2023-10-16 16:10:09 -0400

    Revert "try replace member [slug] with [name=name]"
    This reverts commit 740e8b5a75b310755311978c468d9aeef11a3583.

--- src/routes/[about=about]/[members=members]/[slug]/+page.server.ts
+++ src/routes/[about=about]/[members=members]/[slug]/+page.server.ts
@@ -2,7 +2,7 @@ import { fetchData } from '$lib/helpers/data.js'
 
 export const load = async ({ params }) => {
     const members = await fetchData('team')
-    const member = members.filter(member => member.meta.slug === params.name)
+    const member = members.filter(member => member.meta.slug === params.slug)
     return {
         member
     }
