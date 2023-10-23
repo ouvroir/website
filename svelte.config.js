291f34d276d318d2fe5086669510457e94212f82
William Diakite <william.diakite@gmail.com>
2023-10-22 22:42:59 -0400

    adds url to prerender.includs

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		prerender: {},
+		prerender: {
+			includes: ['*', '/a-propos/membres/emchateau']
+		},
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
 			// base: '/website'
