ecb650a6a47d3844e745eb081b9e132c977eb1c2
William Diakite <william.diakite@gmail.com>
2023-10-22 22:49:37 -0400

    prerender : specify entries for about and a-propos

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
-			entries: ['*', '/a-propos/membres/emchateau']
+			entries: ['*', '/about', '/a-propos']
 		},
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
