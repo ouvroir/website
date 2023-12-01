619624ba29c85865338e5e81e622b3faff786d12
William Diakite <william.diakite@gmail.com>
2023-12-01 11:02:46 -0500

    now copies project images

--- copyAssets.sh
+++ copyAssets.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
 
-# Define the source and target directories
+# Copy profile images
 source_directory="./src/lib/labouvroir/equipe/profile-img"
 target_directory="./static/team"
+
+# Check if source directory exists
+if [ ! -d "$source_directory" ]; then
+  echo "Source directory does not exist."
+  exit 1
+fi
+
+# Check if target directory exists
+if [ ! -d "$target_directory" ]; then
+  echo "Target directory does not exist. Creating it now."
+  mkdir -p "$target_directory"
+fi
+
+# Copy all files
+cp "$source_directory"/* "$target_directory"
+
+# Copy project images
+source_directory="./src/lib/labouvroir/projets/images"
+target_directory="./static/images/projets"
+
+# Check if source directory exists
+if [ ! -d "$source_directory" ]; then
+  echo "Source directory does not exist."
+  exit 1
+fi
+
+# Check if target directory exists
+if [ ! -d "$target_directory" ]; then
+  echo "Target directory does not exist. Creating it now."
+  mkdir -p "$target_directory"
+fi
+
+# Copy all files
+cp "$source_directory"/* "$target_directory"
+
+
+# Copy project images
+source_directory="./src/lib/labouvroir/projets/images/banner"
+target_directory="./static/images/projets/banner"
 
 # Check if source directory exists
 if [ ! -d "$source_directory" ]; then
