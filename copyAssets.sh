3e5a4b04f2488eec71cff4311de9a6b99664ada1
William Diakite <william.diakite@gmail.com>
2023-12-04 15:00:06 -0500

    add recursive copying

--- copyAssets.sh
+++ copyAssets.sh
@@ -36,26 +36,9 @@ if [ ! -d "$target_directory" ]; then
 fi
 
 # Copy all files
-cp "$source_directory"/* "$target_directory"
+cp -r "$source_directory"/* "$target_directory"
 
 
-# Copy project images
-source_directory="./src/lib/labouvroir/projets/images/banner"
-target_directory="./static/images/projets/banner"
 
-# Check if source directory exists
-if [ ! -d "$source_directory" ]; then
-  echo "Source (banner) directory does not exist."
-  exit 1
-fi
-
-# Check if target directory exists
-if [ ! -d "$target_directory" ]; then
-  echo "Target directory does not exist. Creating it now."
-  mkdir -p "$target_directory"
-fi
-
-# Copy all files
-cp "$source_directory"/* "$target_directory"
 
 echo "Files copied successfully!"
